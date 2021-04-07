ea7dfa384315eda5435dd92a1474f9dcd440ed83
Hoziefa Alhassan <hoziefaalhasan@gmail.com>
2021-04-07 21:23:15 +0300

    Make current user type not dependent to make more dependency inversion.

--- src/types/typeAliases.ts
+++ src/types/typeAliases.ts
@@ -1,7 +1,7 @@
-import firebase from 'firebase/app';
+import { CurrentUserFromService } from '../services/FirebaseService';
 
 export type PartialRequired<T, U = { [K in keyof T]: Pick<T, K> }> = Partial<T> & U[keyof U];
 
 export type HTMLElementEvent<T extends Element, C = T> = Event & { target: T; currentTarget: C };
 
-export type CurrentUser = firebase.User | null;
+export type CurrentUser = CurrentUserFromService | null;
